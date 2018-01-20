ecf06d5e87ada9045ee91bff12b041a3852b2547
Minh Thanh <mihthanh27@gmail.com>
2018-01-20 21:49:09 +0700

    Background job

---
+++ app/background/index.js
@@ -0,0 +1,64 @@
+const CronJob = require('cron').CronJob;
+const knex = require('../db');
+const config = require('../config');
+
+module.exports = new CronJob('*/5 * * * *', function() {
+  
+  console.log('Run every 5 min');
+  // Change status from NEW to READY if we got enough member
+  knex.table('challenges')
+    .select('challenges.*, (SELECT COUNT(id) from challengesacceptant where challengesacceptant.challenge_id = challenges.id) as total_member')
+    .where({ status: config.NEW })
+    .then(challenges => {
+      challenges.forEach(challenge => {
+        if (challenge.total_member === challenge.require_user) {
+          // READY
+          return knex.table('challenges')
+            .where({ id: challenge.id })
+            .update({ status: config.READY })
+        }
+        return false;
+      });
+    }).then(data => {
+      if (data) {
+        // Todo: Send notification that challenge success
+      }
+    });
+
+  
+  // Change status from NEW to fail if not enough members
+  knex.table('challenges')
+    .update({ status: config.FAIL })
+    .where('start_time', '<', new Date())
+    .andWhere({ status: config.NEW })
+    .then(challenges => {
+      // Todo: Send notification that challenge fail
+    });
+
+
+  // When end_time passed
+  knex.table('challenges')
+    .select(`challenges.*, (SELECT COUNT(id) FROM challengesacceptant WHERE challengesacceptant.challenge_id = challenges.id AND challengesacceptant = ${config.COMPLETE}) as total_member`)
+    .where('end_time', '<', new Date())
+    .andWhere({ status: config.NEW })
+    .then(challenges => {
+      challenges.forEach(challenge => {
+        if (challenge.total_member === challenge.require_user) {
+          // SUCCESS
+          return knex.table('challenges')
+            .where({ id: challenge.id })
+            .update({ status: config.SUCCESS })
+        } else {
+          // FAIL
+          return knex.table('challenges')
+            .where({ id: challenge.id })
+            .update({ status: config.FAIL })
+        }
+      });
+    }).then(data => {
+      console.log(data);
+    });
+
+  
+
+}, null, true, 'America/Los_Angeles');
--- app/config.js
+++ app/config.js
@@ -7,9 +7,15 @@ module.exports = {
   twilioSid: 'AC6ea33a67a87050af813e34b0ab86a88f',
   twilioToken: 'd1f54e0709268537b0bcda6140b221f6',
   twilioPhone: '+14159434842',
+  // Challenge acceptant status
   ACCEPT: 1,
   DOING: 2,
   COMPLETE: 3,
   GIVEUP: 4,
   WON: 5,
+  // Challenge status
+  NEW: 1,
+  READY: 2,
+  FAIL: 3,
+  SUCCESS: 4,
 }
